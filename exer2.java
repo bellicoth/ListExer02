81eaef47ef952ea2f985cb6f53ade33ad54e993c
bellicoth <88353387+bellicoth@users.noreply.github.com>
2022-04-09 12:25:06 -0300

    Create exer2.java

---
+++ exer2.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+
+class Main {
+  public static void main(String[] args) {
+    /*2. Escreva um programa que leia um conjunto de 10 números inteiros positivos. Seu programa deve determinar
+e imprimir o maior deles. Use o laço FOR.*/
+  
+    Scanner grava = new Scanner(System.in);
+    int x=1;
+    for( x = 1; x <= 10; x++){
+
+      System.out.println(" " + x);
+      
+    }
+    System.out.println("\nO maior é: " + (x-1));
+    
+    
+    
+  }
+}
